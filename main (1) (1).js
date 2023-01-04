10b5ae1a88394ae8fbc275a74c053d805a551047
nixe-123 <106360915+nixe-123@users.noreply.github.com>
2023-01-04 13:18:31 -0600

    Add files via upload

---
+++ main (1) (1).js
@@ -0,0 +1,68 @@
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
+window.addEventListener("Keydown", my_keydown);
+
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image(); //define una variable para una imagen nueva
+	img_imgTag.onload = uploadimg; // establece una función para cargar la variable
+	img_imgTag.src = img_image;   // carga la imagen
+}
+
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+
+function aplhabetkey()
+{
+	img_image="Alpkey.png"
+	add();	
+}
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+		{
+		aplhabetkey();
+		document.getElementById("d1").innerHTML="You pressed alphabet key";
+		console.log("alphabet key")
+	}
+}
+
+function aplhabetkey()
+{
+	img_image="Alpkey.png"
+	add();	
+
+}
+function numberkey()
+{
+	img_image="numkey.png"
+	add();	
+}
+function arrowkey()
+{
+	img_image="Arrkey.png"
+	add();	
+}
+function specialkey()
+{
+	img_image="Alpkey.png"
+	add();	
+}
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
+}
